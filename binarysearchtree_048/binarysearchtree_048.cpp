7103a958f5587fd9ecfc105fd4db6df927939795
rafiazharr17 <rafipilo17@gmail.com>
2023-06-30 22:26:06 +0700

    memesan memory untuk node baru

--- binarysearchtree_048/binarysearchtree_048.cpp
+++ binarysearchtree_048/binarysearchtree_048.cpp
@@ -25,6 +25,6 @@ public:
 	}
 
 	void insert(string element) { //insert a node in the binary search tree
-
+		Node* newNode = new Node(element, NULL, NULL); //Allocate memory for the new node
 	}
 };
