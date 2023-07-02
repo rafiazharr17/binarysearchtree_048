d36da746136478c2a36941dfa33d8b28c98da9c9
rafiazharr17 <rafipilo17@gmail.com>
2023-07-02 18:53:01 +0700

    membuat switch case untuk memilih menu

--- binarysearchtree_048/binarysearchtree_048.cpp
+++ binarysearchtree_048/binarysearchtree_048.cpp
@@ -120,6 +120,42 @@ int main() {
 		cout << "5. Exit" << endl;
 		cout << "\nEnter your choice (1-5) : " << endl;
 
+		char ch;
+		cin >> ch;
+		cout << endl;
 
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word : ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
+		case '2':
+		{
+			obj.inorder(obj.ROOT);
+			break;
+		}
+		case '3':
+		{
+			obj.preorder(obj.ROOT);
+			break;
+		}
+		case'4':
+		{
+			obj.postorder(obj.ROOT);
+			break;
+		}
+		case'5':
+			return 0;
+		default:
+		{
+			cout << "Invalid option" << endl;
+			break;
+		}
+		}
 	}
 }
