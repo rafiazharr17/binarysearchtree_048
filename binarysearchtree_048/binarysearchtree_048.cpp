b7b224e50c2a6e8bb7ceaa3e5851b7b5323ba35e
rafiazharr17 <rafipilo17@gmail.com>
2023-07-02 18:48:04 +0700

    membuat method postorder

--- binarysearchtree_048/binarysearchtree_048.cpp
+++ binarysearchtree_048/binarysearchtree_048.cpp
@@ -79,6 +79,20 @@ public:
 	}
 
 	void preorder(Node* ptr) {
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
+
+	void postorder(Node* ptr) {
 
 	}
 };
